20f4a419c353e3c32b2ea423d5239c9286e1743f
baloran <contact@baloran.fr>
2015-11-21 19:26:01 +0100

    add message for pass the intro + blink this

--- app/controller/Intro.js
+++ app/controller/Intro.js
@@ -6,12 +6,23 @@ var introView = YoloJS.View.extend({
   timingAnimationIntro: 1000,
 
   app: function () {
-    var player = $('.page-'+this.pageName).find('video.video-intro').get(0);
+    $page = $('.page-'+this.pageName);
+    var player = $page.find('video.video-intro').get(0);
     player.play();
+
+    player.onended = function () {
+      endVideoIntro();
+    }
+
+    setTimeout(function () {
+      $page.find('h2').fadeIn();
+    }, 10000)
+
+    $(document).on('keyup', endVideoIntro);
   }
 });
 
 
-function endVideo() {
-  router.navigate('/configuration');
+function endVideoIntro() {
+  Daredevil.router.navigate('/configuration');
 }
