0343fa1b75eac36ae5685aaa1fb44bc63691d67b
baloran <contact@baloran.fr>
2015-11-27 11:30:10 +0100

    fix link intro

--- app/controller/Intro.js
+++ app/controller/Intro.js
@@ -22,6 +22,10 @@ var introView = YoloJS.View.extend({
       $('.skip-btn').css('display', 'block');
     }, 2000);
 
+    $('.skip-btn').click(function (e) {
+      e.preventDefault();
+    });
+
 
     $player.get(0).onended = function () { endVideoIntro(); };
     $('.video-intro').on('click', function (e)
